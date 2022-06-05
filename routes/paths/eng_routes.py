76fa660637e7ccbea0c4822c6b65050233d86222
bjcreal <bjcreal@umich.edu>
2022-06-05 16:34:48 -0400

    eng routes

--- routes/paths/eng_routes.py
+++ routes/paths/eng_routes.py
@@ -5,41 +5,41 @@ import json
 @courseflow.app.route('/eng/hardware')
 def get_eng_hardware():
     path_to_jsons = courseflow.app.config['COURSE_PATH_JSONS']
-    with open(f'/{path_to_jsons}/hardware-schema.json/') as hardware_json:
+    with open(f'/{path_to_jsons}/eng-comhardware-schema.json') as hardware_json:
         hardware_info = json.load(hardware_json)
-    return flask.render_template("lsa-ai.html", **hardware_info)
+    return flask.render_template("eng.html", **hardware_info)
 
 @courseflow.app.route('/eng/infrastructure')
 def get_eng_infrastructure():
     path_to_jsons = courseflow.app.config['COURSE_PATH_JSONS']
-    with open(f'/{path_to_jsons}/infrastructure-schema.json/') as infrastructure_json:
+    with open(f'/{path_to_jsons}/eng-compinfrast-schema.json') as infrastructure_json:
         infrastructure_info = json.load(infrastructure_json)
-    return flask.render_template("lsa-ai.html", **infrastructure_info)
+    return flask.render_template("eng.html", **infrastructure_info)
 
 @courseflow.app.route('/eng/intelligentsystems')
 def get_eng_intelligent_systems():
     path_to_jsons = courseflow.app.config['COURSE_PATH_JSONS']
-    with open(f'/{path_to_jsons}/intelligentsystems-schema.json/') as intelligent_sys_json:
+    with open(f'/{path_to_jsons}/eng-intellsys-schema.json') as intelligent_sys_json:
         intelligent_sys_info = json.load(intelligent_sys_json)
-    return flask.render_template("lsa-ai.html", **intelligent_sys_info)
+    return flask.render_template("eng.html", **intelligent_sys_info)
 
 @courseflow.app.route('/eng/software')
 def get_eng_software():
     path_to_jsons = courseflow.app.config['COURSE_PATH_JSONS']
-    with open(f'/{path_to_jsons}/software-schema.json/') as software_json:
+    with open(f'/{path_to_jsons}/eng-software-dev-schema.json') as software_json:
         software_info = json.load(software_json)
-    return flask.render_template("lsa-ai.html", **software_info)
+    return flask.render_template("eng.html", **software_info)
 
 @courseflow.app.route('/eng/theory')
 def get_eng_theory():
     path_to_jsons = courseflow.app.config['COURSE_PATH_JSONS']
-    with open(f'/{path_to_jsons}/theory-schema.json/') as theory_json:
+    with open(f'/{path_to_jsons}/eng-computation-schema.json') as theory_json:
         theory_info = json.load(theory_json)
-    return flask.render_template("lsa-ai.html", **theory_info)
+    return flask.render_template("eng.html", **theory_info)
 
 @courseflow.app.route('/eng/webtech')
 def get_eng_webtech():
     path_to_jsons = courseflow.app.config['COURSE_PATH_JSONS']
-    with open(f'/{path_to_jsons}/webtech-schema.json/') as web_tech_json:
+    with open(f'/{path_to_jsons}/eng-webtech-schema.json') as web_tech_json:
         web_tech_info = json.load(web_tech_json)
-    return flask.render_template("lsa-ai.html", **web_tech_info)
+    return flask.render_template("eng.html", **web_tech_info)
